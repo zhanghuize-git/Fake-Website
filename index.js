10d357c637cadb3f0c1e04533cc819c6821211cd
It-Smaba <56755115+z954576065@users.noreply.github.com>
2020-04-30 19:38:04 +0800

    firstly-submit

---
+++ index.js
@@ -0,0 +1,43 @@
+const glide = new Glide(".glide");
+const captionsEl=document.querySelectorAll(".slide-caption");
+
+glide.on(["mount.after","run.after"],()=>{
+    const caption =captionsEl[glide.index];
+    anime({
+        targets:caption.children,
+        opacity:[0,1],
+        duration:400,
+        easing:"linear",
+        delay:anime.stagger(400,{start:300}),
+        translateY:[anime.stagger([40,10]),0]
+
+    });
+});
+
+glide.on("run.before",()=>{
+    document.querySelectorAll(".slide-caption >*").forEach(el=>{
+        el.style.opacity=0;
+    })
+})
+
+
+glide.mount();
+
+const isotope =new Isotope(".cases",{
+    layoutMode:"fitRows",
+    itemSelector:".case-item"
+});
+
+const filterBtns =document.querySelector(".filter-btns");
+
+filterBtns.addEventListener("click",e=>{
+    let{target}=e;
+    const filterOption  =target.getAttribute("data-filter");
+    if(filterOption){
+        document.querySelectorAll(".filter-btn.active")
+        .forEach(btn=>btn.classList.remove("active"));
+        target.classList.add("active");
+
+        isotope.arrange({filter:filterOption});
+    }
+})
