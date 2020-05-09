610917be173afb0a68e2cc8f72648f36ab35a81a
It-Smaba <56755115+z954576065@users.noreply.github.com>
2020-05-09 17:13:55 +0800

    2020.5.8日更新“

---
+++ example.js
@@ -0,0 +1,7 @@
+function putInRefrigerator{
+    console.log("打开冰箱门");
+    console.log("把大象放进去");
+    console.log("关上冰箱门");
+}
+
+putInRefrigerator();
--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const headerEl =document.querySelector("header");
+const scrollToTop =document.querySelector(".scrollToTop");
+window.addEventListener("scroll",()=>{
+    let height =headerEl.getBoundingClientRect().height;
+
+    if(window.pageYOffset -height>800){
+        if(!headerEl.classList.contains("sticky")){
+            headerEl.classList.add("sticky");
+        }
+    }else{
+        headerEl.classList.remove("sticky");
+    }
+
+    if(window.pageYOffset>2000){
+        scrollToTop.style.display="block";
+    }else{
+        scrollToTop.style.display ="none";
+    }
+});
+
+
 const glide = new Glide(".glide");
 const captionsEl=document.querySelectorAll(".slide-caption");
 
@@ -41,3 +62,53 @@ filterBtns.addEventListener("click",e=>{
         isotope.arrange({filter:filterOption});
     }
 })
+
+const staggeringOption ={
+    display:300,
+    distance:"50px",
+    duration:500,
+    easing:"ease-in-out",
+    origin:"bottom"
+};
+
+
+ScrollReveal().reveal(".feature",{...staggeringOption,interval:350});
+ScrollReveal().reveal(".service-item",{...staggeringOption,interval:450});
+
+ScrollReveal().reveal(".data-section",{
+    beforeReveal:()=>{
+        anime({
+            targets:".data-piece .num",
+            innerHTML:el =>{
+                return[0,el.innerHTML];
+            },
+            duration:20000,
+            round:1,
+            easing:"easeInExpo"
+        });
+    }
+});
+
+const dataSectionEl =document.querySelector(".data-section");
+window.addEventListener("scroll",()=>{
+    const bottom =dataSectionEl.getBoundingClientRect().bottom;
+    const top = dataSectionEl.getBoundingClientRect().top;
+
+    if(bottom>= 0 && top <= window.innerHeight){
+        dataSectionEl.style.backgroundPosition = 'center calc(50% -${bottom/5}px)';
+    }
+})
+
+const scroll =new SmoothScroll('nav a[href*="#"],.scrollToTop a[href*="#"]',{
+    header:"header",
+    offset:80
+});
+
+// 点击更多按钮跳转
+const exploreBtnELs=document.querySelectorAll(".explore-btn");
+
+exploreBtnELs.forEach(exploreBtnEL =>{
+    exploreBtnEL.addEventListener("click",()=>{
+        scroll.animateScroll(document.querySelector("#about-us"));
+    });
+});
